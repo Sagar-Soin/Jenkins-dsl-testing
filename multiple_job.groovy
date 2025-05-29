fe5df1f4791e76c30cba4146bbcc4de1b714b5db
Your Name <you@example.com>
2025-05-29 04:52:23 +0000

    Second commit another folder

--- multiple_job.groovy
+++ multiple_job.groovy
@@ -6,10 +6,20 @@ Folder(project) {
     microservices.each { services ->
         job("${project}-${services}") {
             description("This is my service: ${project}-${services}")
+            scm{
+                git {
+                    remote{
+                        url('https://github.com/Sagar-Soin/Jenkins-dsl-testing.git')
+                    }
+                    branches('*/master')
+                }
+            }
             steps {
                 shell("echo My Project is ready : ${project}-${services}")
             }
         }
     }
 }
+
+
 
